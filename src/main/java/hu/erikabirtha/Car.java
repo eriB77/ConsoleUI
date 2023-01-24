8b55474e2746ce102f306f5e0296d473a7dcbd21
Erika Birtha <ebirtha@hotmail.com>
2023-01-24 07:47:11 +0100

    lekérdezés kiiratás

--- src/main/java/hu/erikabirtha/Car.java
+++ src/main/java/hu/erikabirtha/Car.java
@@ -6,4 +6,15 @@ public class Car {
     public String model;
     public Integer numberOfSeats;
     public String vehicleType;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "registrationNumber='" + registrationNumber + '\'' +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", numberOfSeats=" + numberOfSeats +
+                ", vehicleType='" + vehicleType + '\'' +
+                '}';
+    }
 }
